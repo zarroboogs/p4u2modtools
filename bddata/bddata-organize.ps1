4719e7c416d01299fc0b8b50a6b371e40ae9c258
zarroboogs <36057965+zarroboogs@users.noreply.github.com>
2018-07-21 21:05:12 +0300

    added bddata.bin tools

---
+++ bddata/bddata-organize.ps1
@@ -0,0 +1,18 @@
+﻿
+$target = if ($args[0]) { $args[0] } else { "bddata-toc.txt" }
+
+$files = gci "bddata+*"
+
+# make dirs
+$dirs = Split-Path $target -Parent | group
+
+foreach ($dir in $dirs.Name) {
+    if (!(Test-Path $dir)) {
+        mkdir $dir -Verbose
+    }
+}
+
+# move files
+for ($i=0; $i -lt $files.Count; $i++) {
+    move-item $files[$i] $target[$i] -Verbose
+}
---
+++ bddata/eboot-extract-toc.ps1
@@ -0,0 +1,9 @@
+﻿
+$eboot = if ($args[0]) { $args[0] } else { "boot.elf" }
+$toc = if ($args[1]) { $args[1] } else { "bddata-toc.txt" }
+
+gc $eboot |
+    select-string -pattern "data\/[a-z0-9\/_.]+\.[a-z0-9]+" -allmatches |
+    % { $_.matches.value } |
+    ? { $_ -notmatch "_appendlist" } `
+    > $toc
