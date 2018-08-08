3fc5d434abca2e9492767f70027bf86af5dc19d6
zarroboogs <36057965+zarroboogs@users.noreply.github.com>
2018-08-08 15:52:50 +0300

    fix organize script

--- bddata/bddata-organize.ps1
+++ bddata/bddata-organize.ps1
@@ -2,17 +2,18 @@
 $target = if ($args[0]) { $args[0] } else { "bddata-toc.txt" }
 
 $files = gci "bddata+*"
+$content = gc $target
 
 # make dirs
-$dirs = Split-Path $target -Parent | group
+$dirs = Split-Path $content -Parent | group
 
 foreach ($dir in $dirs.Name) {
-    if (!(Test-Path $dir)) {
+    if ($dir -and !(Test-Path $dir)) {
         mkdir $dir -Verbose
     }
 }
 
 # move files
 for ($i=0; $i -lt $files.Count; $i++) {
-    move-item $files[$i] $target[$i] -Verbose
+    move-item $files[$i] $content[$i] -Verbose
 }
